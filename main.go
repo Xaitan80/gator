dc80e127df620954d5d10f4802ff2676d46c58a0
Xaitan <utrustad.sprang_0z@icloud.com>
2025-08-28 20:02:13 +0200

    lesson 2

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/xaitan80/gator/internal/cli"
 	"github.com/xaitan80/gator/internal/config"
 )
 
@@ -14,16 +16,30 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Set current user
-	if err := cfg.SetUser("christian"); err != nil {
-		log.Fatal(err)
+	// Initialize state
+	appState := &cli.State{
+		Config: &cfg,
 	}
 
-	// Read again to confirm
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatal(err)
+	// Initialize commands and register handlers
+	cmds := &cli.Commands{}
+	cmds.Register("login", cli.HandlerLogin)
+
+	// Check command-line arguments
+	if len(os.Args) < 2 {
+		fmt.Println("Error: not enough arguments")
+		os.Exit(1)
 	}
 
-	fmt.Printf("%+v\n", cfg)
+	// Split command name and arguments
+	cmd := cli.Command{
+		Name: os.Args[1],
+		Args: os.Args[2:],
+	}
+
+	// Run the command
+	if err := cmds.Run(appState, cmd); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 }
